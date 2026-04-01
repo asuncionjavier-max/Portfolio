68ec602d04a182a9e6461e282736fa2567c77630
Javi <asuncionjavier96@gmail.com>
2026-04-02 00:25:14 +0200

    añadimos la funcion de tema claro/oscuro

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,9 @@
-console.log('Hello!');
+
+    const button = document.getElementById('dark-light')
+    const light = document.querySelector('body')
+
+    button.addEventListener("click", function cambioColor(event){
+    event.preventDefault();
+    console.log("El botón funciona y la clase se está activando");
+    light.classList.toggle('dark-mode')
+    })
