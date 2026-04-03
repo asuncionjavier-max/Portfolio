65561c11e7857ba5e0e37f1f3ddb4c950b20187b
Javi <asuncionjavier96@gmail.com>
2026-04-03 11:43:18 +0200

    añadimos interaccion con imagen sol y luna en modo noche

--- JS/script.js
+++ JS/script.js
@@ -1,9 +1,18 @@
 
     const button = document.getElementById('dark-light')
-    const light = document.querySelector('body')
+    const body = document.querySelector('body')
+    const header = document.getElementById('header')
+    const moonSun = document.getElementById('icono-visual')
+    const keyframes = document.querySelector('proyectos')
 
     button.addEventListener("click", function cambioColor(event){
     event.preventDefault();
-    console.log("El botón funciona y la clase se está activando");
-    light.classList.toggle('dark-mode')
-    })
+    body.classList.toggle('light-mode')
+    if(body.classList.contains('light-mode')){
+     moonSun.src = "https://res.cloudinary.com/dbsvmqrm6/image/upload/v1775120337/1518052_cn6lgn.png"
+     keyframes.classList.toggle('anim-light')
+    }
+else{
+    moonSun.src = "https://res.cloudinary.com/dbsvmqrm6/image/upload/v1775079674/sun-white-icon-vector_ppjaus.jpg"
+}
+})
